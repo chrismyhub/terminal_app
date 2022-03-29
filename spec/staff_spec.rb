edb00358d6047c2461fda62f88bafba5bf012168
chrismyhub <keyoch@gmail.com>
2022-03-29 14:57:29 +1100

    subject the staff being tested

--- spec/staff_spec.rb
+++ spec/staff_spec.rb
@@ -1,13 +1,13 @@
 require './staff'
 
 describe Staff do
+  subject(:staff) {Staff.new('Peterpan1', 'Peter Pan', 'TEAM_MEMBER_MAX_LEAVE_ALLOCATED', 'abc123')}
+  
   it 'can be instantiated' do
-    staff = Staff.new('Peterpan1', 'Peter Pan', 'TEAM_MEMBER_MAX_LEAVE_ALLOCATED', 'abc123' )
     expect(staff).not_to be_nil
     expect(staff).to be_instance_of Staff
   end
   it 'returns staffid' do
-    staff = Staff.new('Peterpan1', 'Peter Pan', 'TEAM_MEMBER_MAX_LEAVE_ALLOCATED', 'abc123' )
     expect(staff.staffid).to eq 'Peterpan1'
   end
 
