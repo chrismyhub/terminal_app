aa9d20fef39fe547c2276d87a9da526a4f824c22
chrismyhub <keyoch@gmail.com>
2022-03-29 14:37:22 +1100

    staff not nill and an instance of Staff

--- spec/staff_spec.rb
+++ spec/staff_spec.rb
@@ -3,6 +3,8 @@ require './staff'
 describe Staff do
     it 'can be instantiated' do
         staff = Staff.new("Peterpan1", "Peter Pan", "TEAM_MEMBER_MAX_LEAVE_ALLOCATED", "abc123" )
+        expect(staff).not_to be_nil
+        expect(staff).to be_instance_of Staff
     end
 
 end
