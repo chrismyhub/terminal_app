5711a0601f8bffcca64eb93c1b04edbadaa00827
chrismyhub <keyoch@gmail.com>
2022-03-30 16:54:18 +1100

    leave class leave not nill pass

--- spec/leave_spec.rb
+++ spec/leave_spec.rb
@@ -4,7 +4,7 @@ describe Leave do
   let(:leave) { Leave.new('Peterpan1', 'TEAM_MEMBER_MAX_LEAVE_ALLOCATED', 2, 18) }
 
   it 'can be instantiated' do
-    expect(leave).not_to be_nil
+    expect(leave).not_to be_nil 
   end
 
 
