8edf65cfe82b5950cc60cacf963db47ed6eae4a8
chrismyhub <keyoch@gmail.com>
2022-03-30 16:52:12 +1100

    leave class unit test instaniated pass

--- leave.rb
+++ leave.rb
@@ -1,3 +1,8 @@
 class Leave
-
+  def initialize(staffid, role, leave_taken, leave_remaining)
+    @staffid = staffid
+    @role = role
+    @leave_taken = leave_taken
+    @leave_remaining = leave_remaining
+  end
 end
