63d72d5db92285f763cc43373d6a19447ab3db1b
chrismyhub <keyoch@gmail.com>
2022-04-02 14:11:00 +1100

    password feature now working

--- staff.rb
+++ staff.rb
@@ -18,13 +18,13 @@ class Staff
     READ_STAFF_FILE.find { |values| values.include?(staffid)}[column_index]
   end
 
-  # WRITE METHOD FOR UPDATE NAME
+
   def self.input_name(new_name)
     puts "Please enter your#{new_name}name:"
     UserInput.entry.capitalize
   end
 
-  # WRITE A METHOD FOR DELETE STAFF ID (WHEN I DELETE A STAFF OFF THE CSV FILE)
+  
   def self.create_id(new_staff_name)
     "#{new_staff_name.gsub(/[[:space:]]/, '')}1"
   end
@@ -47,18 +47,18 @@ class Staff
     return max_leave_allocated
   end
 
-  # CREATE METHOD FOR UPDATE PASSWORD (DELETING OF PASSWORD HANDLED BY DELETE STAFF)
-  def self.input_password
-    puts 'Please enter a password:'
+
+  def self.input_password(new_password)
+    puts "Please enter #{new_password} password:"
     UserInput.entry
   end
 
-  # CREATE NEW METHOD FOR UPDATE AND DELETE EXISTING STAFF DETAILS 
+ 
   def self.create_new
     new_staff_name = input_name(' ')
     new_staff_id = create_id(new_staff_name)
     new_staff_role = input_role
-    new_staff_password = input_password
+    new_staff_password = input_password('a')
     Staff.new(new_staff_id, new_staff_name, new_staff_role, new_staff_password)
   end
 
@@ -82,74 +82,119 @@ class Staff
   def self.update_name(staffid)
     new_name = input_name(" new ")
 
-    # FIND EXISTING ROLE
     staff_role = find_staff_in_csv(staffid, 2)
     staff_password = Validation.find_password_from_csv(staffid)
-  
-    # OPEN STAFF CSV File
+
     CSV.open('staff_output.csv', 'a') do |csv|
-    # INSERT NEW NAME AND ADD NEW STAFF DETAILS TO STAFF OUTPUT CSV
     csv << [staffid, new_name, staff_role, staff_password]
     end
-    # add_to_staff_csv('staff_output.csv', staff_role)
 
-    # DELETE OLD STAFF DETAILS
-
-    # CALL STAFF CSV FILE
-    data_staff = CSV.table('staff.csv')
-
-    # DELETE ROW IF STAFF NAME STAFF ID
-    data_staff.delete_if do |row|
+    EDIT_STAFF_FILE.delete_if do |row|
     row[:staffid] == staffid
     end
 
-    # WRITE UPDATED STAFF CSV FILE
     File.open('staff.csv', 'w') do |f|
-    f.write(data_staff.to_csv)
+    f.write(EDIT_STAFF_FILE.to_csv)
     end
 
-    # MOVE UPDATED DETAILS TO EXISTING LIST
-
-    # OPEN STAFF OUTPUT CSV File
     data_staff_new = CSV.read('staff_output.csv')
-    # LOOK FOR EXISTING STAFF ID FROM OUTPUT CSV
+
     staff_id_new = data_staff_new.find { |id| id.include?(staffid)}[0]
-    # LOOK FOR UPDATED NAME FROM OUTPUT CSV
+
     staff_name_new = data_staff_new.find { |name| name.include?(staffid)}[1]
-    # LOOK FOR EXISTING ROLE FROM OUTPUT CSV
+
     staff_role_new = data_staff_new.find { |role| role.include?(staffid)}[2]
-    # LOOK FOR EXISTING PASSWORD FROM OUTPUT CSV
+
     staff_password_new = data_staff_new.find { |password| password.include?(staffid)}[3]
-    
-    #OPEN EXISTING STAFF CSV
+
     CSV.open('staff.csv', 'a') do |csv|
-    # COPY UPDATED DETAILS FROM OUTPUT CSV TO EXISTING STAFF CSV
+
     csv << [staff_id_new, staff_name_new, staff_role_new, staff_password_new]
     end
 
-
-    # DELETE OUTPUT CSV CONTENTS
-
-    # CALL STAFF OUTPUT CSV FILE
     data_staff = CSV.table('staff_output.csv')
 
-    # DELETE ROW IF STAFF NAME STAFF ID IN STAFF OUTPUT CSV
     data_staff.delete_if do |row|
     row[:staffid] == staffid
     end
 
-    # WRITE UPDATED STAFF OUTPUT CSV FILE
     File.open('staff_output.csv', 'w') do |f|
     f.write(data_staff.to_csv)
     end
 
-    puts "You have successfully changed your Name!"
+    puts 'You have successfully changed your Name!'
     Validation.return_to_menu("Main")
     Main.run
 
   end
 
-  
+  def self.update_password(staffid)
+    new_password = input_password('new')
+
+    staff_role = find_staff_in_csv(staffid, 2)
+    staff_name = find_staff_in_csv(staffid, 1)
+
+    # OPEN STAFF OUTPUT CSV File
+    CSV.open('staff_output.csv', 'a') do |csv|
+      # INSERT NEW NAME AND ADD NEW STAFF DETAILS TO STAFF OUTPUT CSV
+      csv << [staffid, staff_name, staff_role, new_password]
+      end
+
+      # DELETE OLD STAFF DETAILS
+
+      # CALL STAFF CSV FILE
+      # data_staff = CSV.table('staff.csv')
+
+      # DELETE ROW IF STAFF NAME STAFF ID
+      EDIT_STAFF_FILE.delete_if do |row|
+      row[:staffid] == staffid
+      end
+
+      # WRITE UPDATED STAFF CSV FILE
+      File.open('staff.csv', 'w') do |f|
+      f.write(EDIT_STAFF_FILE.to_csv)
+      end
+
+      # MOVE UPDATED DETAILS TO EXISTING LIST
+
+      # OPEN STAFF OUTPUT CSV File
+      data_staff_new = CSV.read('staff_output.csv')
+      # LOOK FOR EXISTING STAFF ID FROM OUTPUT CSV
+      staff_id_new = data_staff_new.find { |id| id.include?(staffid)}[0]
+      # LOOK FOR UPDATED NAME FROM OUTPUT CSV
+      staff_name_new = data_staff_new.find { |name| name.include?(staffid)}[1]
+      # LOOK FOR EXISTING ROLE FROM OUTPUT CSV
+      staff_role_new = data_staff_new.find { |role| role.include?(staffid)}[2]
+      # LOOK FOR EXISTING PASSWORD FROM OUTPUT CSV
+      staff_password_new = data_staff_new.find { |password| password.include?(staffid)}[3]
+      
+      # OPEN STAFF CSV 
+      CSV.open('staff.csv', 'a') do |csv|
+      # COPY UPDATED DETAILS FROM OUTPUT CSV TO EXISTING STAFF CSV
+      csv << [staff_id_new, staff_name_new, staff_role_new, staff_password_new]
+      end
+
+      # DELETE OUTPUT CSV CONTENTS
+
+      # CALL STAFF OUTPUT CSV FILE
+      data_staff = CSV.table('staff_output.csv')
+
+      # DELETE ROW IF STAFF NAME STAFF ID IN STAFF OUTPUT CSV
+      data_staff.delete_if do |row|
+      row[:staffid] == staffid
+      end
+
+      # WRITE UPDATED STAFF OUTPUT CSV FILE
+      File.open('staff_output.csv', 'w') do |f|
+      f.write(data_staff.to_csv)
+      end
+
+      puts "You have successfully changed your Password!"
+      Validation.return_to_menu("Main")
+      Main.run
+  end
+
+
 
   def self.update_existing(staffid)
     name_or_password = ' '
@@ -160,7 +205,7 @@ class Staff
       if name_or_password == 'N'
         update_name(staffid)
       elsif name_or_password == 'P'
-        puts "will update password"
+        update_password(staffid)
       else
         system 'clear'
         puts "Invalid entry, please try again."
